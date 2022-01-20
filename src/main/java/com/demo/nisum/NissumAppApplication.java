0e2a20e854309f62da2fd2b7066bd13db3597b10
Francisco Briceño <cisco505@MacBook-Pro-de-Francisco.local>
2022-01-19 21:54:11 -0600

    Commit inicial.
    Proyecto demostrativo, ejercicio Nisum

---
+++ src/main/java/com/demo/nisum/NissumAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.nisum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NissumAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NissumAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/nisum/NissumAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.nisum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NissumAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
